2f67a56cc44191cc568b1abbc0e278757ed76733
dwiky hardiansah <dwikyhardiansah93@gmail.com>
2019-02-06 18:11:36 +0700

    Pertamax

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Halo dunia"/> -->
+    <h1>{{ nama }}</h1>
+  <h2>Nama : <input v-model="nama" type="" name=""><br></h2>
+  <h2>Nilai Anda : {{  nilai }}</h2>
+  <button @click = "totalnilai">Click Me</button>
+  <h2>1. Siapa Presiden Amerika Serikat Saat Ini ?</h2>
+  <input type="radio" name="" v-model="soal1" value="Obama">Barrack Obama
+  <input type="radio" name="" v-model="soal1" value="George Washington">George Washington
+  <input type="radio" name="" v-model="soal1" value="Abraham Lincoln">Abraham Lincoln
+  <input type="radio" name="" v-model="soal1" value="Trump">Donald Trump<br>
+  <h2>2. Apakah arti "ПРИВЕТ" Dari Bahasa Rusia ?</h2>
+  <input type="radio" name="" v-model="soal2" value="Halo">Halo
+  <input type="radio" name="" v-model="soal2" value="Dadah">Dadah
+  <input type="radio" name="" v-model="soal2" value="Selamat Malam">Selamat Malam
+  <input type="radio" name="" v-model="soal2" value="Selamat Makan">Selamat Makan<br>
+  <h2>3. Siapakah Presiden Pertama Indonesia ?</h2>
+  <input type="radio" name="" v-model="soal3" value="Soekarno">Soekarno
+  <input type="radio" name="" v-model="soal3" value="Habibie">B.J Habibie
+  <input type="radio" name="" v-model="soal3" value="Megawati">Megawati Soekarnoputri
+  <input type="radio" name="" v-model="soal3" value="Abdurrahman Wahid">Abdurrahman Wahid<br>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
   }
+}*/
+export default{
+  name: 'App',
+  data() {
+    return{
+      nama: '',
+      nilai: 0,
+      soal1: '',
+      soal2: '',
+      soal3: ''
+    }
+},
+methods: {
+    totalnilai(){
+      if(this.soal1 == 'Trump' && this.soal2 == 'Halo' && this.soal3 == 'Soekarno') {
+        this.nilai=+3
+      }
+      else if (this.soal1 == 'Trump' && this.soal2 == 'Halo') {
+        this.nilai=+2
+      }
+      else if (this.soal1 == 'Trump' && this.soal3 == 'Soekarno') {
+        this.nilai=+2
+      }
+      else if (this.soal2 == 'Halo' && this.soal3 == 'Soekarno') {
+        this.niali=+2
+      }
+      else if (this.soal1 == 'Trump') {
+        this.nilai=+1
+      }
+      else if (this.soal2 == 'Halo') {
+        this.nilai=+1
+      }
+      else if (this.soal3 == 'Soekarno') {
+        this.nilai=+1
+      }
+      else{
+        this.nilai = 0
+      }
+    }
+  }
 }
 </script>
 
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'AveHelloWorldnir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,33 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>{{ judul }}</h2>
+    <input v-model="msg"><br><br>
+    <button @click="gantijudul">Ganti Judul</button>
+    <br><br>
+    <input v-model="x" type="" name="">
+    <h1 v-if="x % 2 === 0">Ini Angka Genap</h1>
+    <h1 v-else>Ini Angka Ganjil</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      msg: 'mantul',
+      judul: 'Makin Mantul',
+      x: 2
+    }
+  },
+  mounted(){
+    this.judul = "INI PELAJARAN WEB DINAMIS"
+  },
+  methods:{
+    gantijudul(){
+      this.judul = 'pelajaran web'
+    }
   }
 }
 </script>
